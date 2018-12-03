b5ab56abd4091030b97ae219b456798890ef3d82
Julian Kotrba <me@juliankotrba.xyz>
2018-12-03 15:00:08 +0100

    Improve performance
    However, code is still very unreadable.

--- day3/day3.hs
+++ day3/day3.hs
@@ -1,7 +1,12 @@
 import InputHelper
 import Data.List.Split
 import Data.List
-import qualified Data.Set as S (Set,empty,member,insert, fromList, size)
+import qualified Data.Set as S (Set,empty,member,insert, fromList, size, delete, toList)
+import qualified Data.Map.Strict as M (Map, empty, insert,fromList,member,adjust,filter,elems)
+
+main = do
+    part1
+    part2
 
 part1 = do
     content <- readFile "input.txt"
@@ -73,9 +78,9 @@ toClaim s =
 part2 = do
     content <- readFile "input.txt"
     let input = lines $ content
-    let all = map allCoordinates $ map toClaim input
-    let mapped = map (\xs -> map (\ys -> if (anyDifferent xs ys && xs /= ys) then [] else xs) all) all
-    print (filter (not .containsEmpty) mapped !! 0 !!0 )
+    let allDuplication = S.toList $ (countOverlaps2 (foldr (++) [] $ map allCoordinates2 $ map toClaim input))
+    let uniqueDuplication = foldr (++) [] $ M.elems $ M.filter (\v-> (length v) > 1) (intoMap $ (foldr (++) [] $ map allCoordinates2 $ map toClaim input))
+    print ((allDuplication\\uniqueDuplication) !!0)
 
 containsEmpty :: [[a]] -> Bool
 containsEmpty xs = any (\ys -> length ys == 0) xs 
@@ -83,11 +88,67 @@ containsEmpty xs = any (\ys -> length ys == 0) xs
 anyDifferent :: (Eq a) => [a] -> [a] -> Bool
 anyDifferent a = not . null . intersect a
 
+intoMap :: [(String,Int,Int)] -> M.Map (Int, Int) [String]
+intoMap cs = intoMapHelper cs M.empty
+
+intoMapHelper :: [(String,Int,Int)] -> M.Map (Int, Int) [String] -> M.Map (Int, Int) [String]
+intoMapHelper [] currentMap = currentMap
+intoMapHelper cs currentMap =
+    let 
+        h = head cs 
+        id = idFromTriple h
+        justCoords = coordsFromTriple h
+    in
+        if (M.member justCoords currentMap) then intoMapHelper (tail cs) (M.adjust (id :) justCoords currentMap)
+        else intoMapHelper (tail cs) (M.insert justCoords [id] currentMap)    
+
+allCoordinates2 :: Claim -> [(String,Int,Int)]
+allCoordinates2 c = 
+    let
+        all = allCoordiantesHelper c (height c) []
+    in   
+        allCoordiantesHelper2 c (height c) []
+ 
+allCoordiantesHelper2 :: Claim -> CurrentHeight -> [(String,Int,Int)] -> [(String,Int,Int)]
+allCoordiantesHelper2 _ 0 cc = cc
+allCoordiantesHelper2 c ch cc = 
+    let
+        coordiantes = [((Main.id c),x,(startY c) + ch -1) | x <- [(startX c) .. (startX c) + (width c) -1] ]
+    in
+        allCoordiantesHelper2 c (ch-1) cc++coordiantes
+
+        
+countOverlaps2 :: [(String,Int,Int)] -> S.Set String
+countOverlaps2 cs =
+    let
+        allIds = S.fromList (map (\(id,_,_) -> id) cs)
+    in
+        countOverlapsHelper2 cs allIds S.empty
+
+        
+countOverlapsHelper2 :: [(String,Int,Int)] -> S.Set String -> S.Set (Int,Int) -> S.Set String
+countOverlapsHelper2 [] ids _  = ids
+countOverlapsHelper2 cs ids visited = 
+    let
+        h = head cs 
+        id = idFromTriple h
+        justCoords = coordsFromTriple h
+    in
+        if (S.member justCoords visited) 
+                then 
+                    countOverlapsHelper2 (tail cs) (S.delete id ids) visited
+                else 
+                    countOverlapsHelper2 (tail cs) ids (S.insert justCoords visited) 
+
+idFromTriple :: (String, Int, Int) -> String
+idFromTriple (id,x,y) = id
+
+coordsFromTriple :: (String, Int, Int) -> (Int, Int)
+coordsFromTriple (id,x,y) = (x,y)                   
+
+
 simpleInput1 = "#1 @ 1,3: 4x4"
 simpleInput2 = "#2 @ 3,1: 4x4"
 simpleInput3 = "#3 @ 5,5: 2x2" 
 
 allSimpleInputs = [simpleInput1, simpleInput2, simpleInput3] 
-
- 
- 
